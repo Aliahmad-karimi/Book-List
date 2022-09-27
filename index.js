d460abd2c3f0716b59d35249b2421aa66dda268d
Aliahmad Karimi <Aliahmad-karimi@users.noreply.github.com>
2022-09-27 14:29:03 +0430

    fix linter errors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import Library from "./modules/BookList.js";
-import * as navBar from "./modules/navBar.js";
-import { date } from "./modules/time.js";
+import Library from './modules/BookList.js';
+import * as navBar from './modules/navBar.js';// eslint-disable-line
+import { date } from './modules/time.js';// eslint-disable-line
+
 const BookListadd = new Library();
 BookListadd.addBook();
 BookListadd.renderBooks();
